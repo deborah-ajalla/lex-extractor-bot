a989c50bbc7f37b68e878719a82efabf846a72f1
Deborah Ajalla <deborahajalla@gmail.com>
2026-03-20 13:51:28 -0300

    feat: se implementa carga segura de rutas con dotenv

--- app_v4.py
+++ app_v4.py
@@ -5,9 +5,17 @@ import pytesseract
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+#-----------------------------------------------------
+#PARA EJECUTAR: EN CONSOLA --> streamlit run app_v4.py
+#-----------------------------------------------------
 
 # Carga las variables ocultas del archivo .env
 load_dotenv()
 
-st.title("⚖️ Analizador de Sentencias Judiciales")
+st.title("⚖️ Analizador de Sentencias Judiciales ⚖️")
 st.write("Sube tu fallo en formato PDF para analizar automáticamente los autos, la resolución, la doctrina y más.")
+
+# Lee la ruta de Tesseract desde el .env
+ruta_tesseract = os.getenv("TESSERACT_PATH")
+if ruta_tesseract:
+    pytesseract.pytesseract.tesseract_cmd = ruta_tesseract
